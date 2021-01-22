a9ab29ee26649d0fb905078ead00345bd4b93f3f
Kyle Christenson <Kwatonna@gmail.com>
2021-01-22 01:38:29 -0800

    preparing country riderect buttons

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 var button = document.getElementById("passButton");
+var gerButton = document.getElementById("placeholder3");
+document.getElementById("placeholder3").addEventListener("click", function(){
+    window.location = "countries/Germany.html";
+})
