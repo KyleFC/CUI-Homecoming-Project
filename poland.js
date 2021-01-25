7c448295ee78a83765fb29a1a6f015183a615dbd
Kyle Christenson <Kwatonna@gmail.com>
2021-01-24 16:12:16 -0800

    added button fucntions to Poland.html

---
+++ poland.js
@@ -0,0 +1,8 @@
+document.getElementById("pmap").addEventListener("click", function() {
+    if ((navigator.platform.indexOf("iPhone") != -1) || 
+       (navigator.platform.indexOf("iPad") != -1) || 
+       (navigator.platform.indexOf("iPod") != -1))
+    window.open("maps://maps.google.com/maps/place/Poland");
+    else
+    window.open("https://www.google.com/maps/place/Poland");
+});
