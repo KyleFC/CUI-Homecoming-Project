ccfff6362bf8d7a882b99404cc1a913459e28558
Kyle Christenson <Kwatonna@gmail.com>
2021-01-24 16:11:45 -0800

    added button functions to austra.html

---
+++ austria.js
@@ -0,0 +1,11 @@
+document.getElementById("amap").addEventListener("click", function() {
+    if ((navigator.platform.indexOf("iPhone") != -1) || 
+       (navigator.platform.indexOf("iPad") != -1) || 
+       (navigator.platform.indexOf("iPod") != -1))
+    window.open("maps://maps.google.com/maps/place/Austria");
+    else
+    window.open("https://www.google.com/maps/place/Austria");
+});
+document.getElementById("abutton").addEventListener("click", function() {
+    window.open("Italy.html");
+})
