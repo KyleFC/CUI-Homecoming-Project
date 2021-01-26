88210f824fea7a943bc9913dd7f1101797922384
Kyle Christenson <Kwatonna@gmail.com>
2021-01-25 22:01:30 -0800

    renamed index.js since it is being used for a different page

---
+++ stampPage.js
@@ -0,0 +1,16 @@
+//Germany
+document.getElementById("placeholder3").addEventListener("click", function(){
+    window.location = "countries/Germany.html";
+})
+//Austria
+document.getElementById("placeholder4").addEventListener("click", function(){
+    window.location = "countries/Austria.html";
+})
+//Italy
+document.getElementById("placeholder5").addEventListener("click", function(){
+    window.location = "countries/Italy.html";
+})
+//Poland
+document.getElementById("placeholder6").addEventListener("click", function(){
+    window.location = "countries/Poland.html";
+})
